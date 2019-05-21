5710afa734bc9f95f6742b52bf1502e717f9a12d
firmangel8 <firmangel8@gmail.com>
2019-05-21 08:38:06 +0800

    reconfig data emit

--- socket-server/app.js
+++ socket-server/app.js
@@ -35,7 +35,7 @@ io.on("connection", socket => {
   if (interval) {
     clearInterval(interval);
   }
-  interval = setInterval(() => getApiAndEmit(socket), 3000);
+  interval = setInterval(() => getApiAndEmit(socket), 2000);
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
